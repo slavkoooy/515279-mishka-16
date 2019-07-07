242ad0eee21110d0bb52fa00edac3f232d0622e4
Vyacheslav Hohlov <slava13894@gmail.com>
2019-07-07 19:28:56 +0300

    Добавляет модальное окно, ретину, ховеры, ПП

---
+++ source/js/main.js
@@ -0,0 +1,18 @@
+let modal = document.querySelector('.modal');
+let showModal = document.querySelectorAll('.catalog__item__buy');
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.add('modal--opened');
+  });
+}
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains('modal--opened')) {
+      evt.preventDefault();
+      modal.classList.remove('modal--opened');
+    }
+  }
+});
